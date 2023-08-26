04cacbb99b2008995cece80cc79843a73d339580
rubimeryka <erykarubim13@gmail.com>
2023-08-26 12:42:57 -0300

    1. Testa constr/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,20 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="1",
+        product_name="Paper",
+        company_name="Dunder Mifflin",
+        manufacturing_date="2013-02-28",
+        expiration_date="2023-12-31",
+        serial_number="13 13 13 13",
+        storage_instructions="não rasgar",
+    )
+    assert product.id == '1'
+    assert product.product_name == 'Paper'
+    assert product.company_name == 'Dunder Mifflin'
+    assert product.manufacturing_date == '2013-02-28'
+    assert product.expiration_date == '2023-12-31'
+    assert product.serial_number == '13 13 13 13'
+    assert product.storage_instructions == 'não rasgar'
