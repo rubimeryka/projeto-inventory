0f34ad85967ab0b1761551a330ee6fefb39eb7b6
rubimeryka <erykarubim13@gmail.com>
2023-08-26 12:52:40 -0300

    2. Testa o relatório individual gerado por Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -8,7 +8,7 @@ def test_create_product() -> None:
         company_name="Dunder Mifflin",
         manufacturing_date="2013-02-28",
         expiration_date="2023-12-31",
-        serial_number="13 13 13 13",
+        serial_number="123 456 789",
         storage_instructions="não rasgar",
     )
     assert product.id == '1'
@@ -16,5 +16,5 @@ def test_create_product() -> None:
     assert product.company_name == 'Dunder Mifflin'
     assert product.manufacturing_date == '2013-02-28'
     assert product.expiration_date == '2023-12-31'
-    assert product.serial_number == '13 13 13 13'
+    assert product.serial_number == '123 456 789'
     assert product.storage_instructions == 'não rasgar'
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,22 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="1",
+        product_name="Paper",
+        company_name="Dunder Mifflin",
+        manufacturing_date="2013-02-28",
+        expiration_date="2023-12-31",
+        serial_number="123 456 789",
+        storage_instructions="não rasgar",
+    )
+    assert str(product) == (
+        f"The product {product.id} - {product.product_name} "
+        f"with serial number {product.serial_number} "
+        f"manufactured on {product.manufacturing_date} "
+        f"by the company {product.company_name} "
+        f"valid until {product.expiration_date} "
+        "must be stored according to the following instructions: "
+        f"{product.storage_instructions}."
+    )
