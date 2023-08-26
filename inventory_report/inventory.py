bc1f283a826af6918b459d76fa00ca38d88c9702
rubimeryka <erykarubim13@gmail.com>
2023-08-26 13:53:15 -0300

    5. Crie a classe Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,16 @@
+from inventory_report.product import Product
+from typing import List, Optional
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        if data is None:
+            data = []
+        self._data = data
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
+
+    def add_data(self, data: List[Product] = None) -> None:
+        self._data.extend(data or [])
