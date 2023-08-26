3b819f8331baf2141bba398f36a74b65e145b2ad
rubimeryka <erykarubim13@gmail.com>
2023-08-26 13:25:35 -0300

    4. Crie a classe JsonImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,7 @@
 from typing import Dict, Type, List
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
+import json
 
 
 class Importer(ABC):
@@ -12,8 +13,24 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        with open(self.path, "r") as file:
+            json_data = json.load(file)
+            list_of_products = []
+            for product in json_data:
+                list_of_products.append(
+                    Product(
+                        id=product["id"],
+                        product_name=product["product_name"],
+                        company_name=product["company_name"],
+                        manufacturing_date=product["manufacturing_date"],
+                        expiration_date=product["expiration_date"],
+                        serial_number=product["serial_number"],
+                        storage_instructions=product["storage_instructions"],
+                    )
+                )
+            return list_of_products
 
 
 class CsvImporter:
